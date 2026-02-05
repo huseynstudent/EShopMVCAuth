ea724f7be279502c4e33cf9b134c8467446ae533
Seyidmammadli_i <Seyidmammadli_i@STHQ0128-01>
2026-02-05 21:10:23 +0400

    Add project files.

---
+++ AspnetMvcAuth/Context/AcademyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AspnetMvcAuth.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspnetMvcAuth.Context;
+
+public class AcademyDbContext : IdentityDbContext<ApplicationUser>
+{
+    public AcademyDbContext(DbContextOptions<AcademyDbContext> options) : base(options)
+    {
+    }
+
+}
---
+++ AspnetMvcAuth/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AspnetMvcAuth.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AspnetMvcAuth.Controllers;
+[Authorize]
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [AllowAnonymous]
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
